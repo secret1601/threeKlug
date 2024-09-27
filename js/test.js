a28eb472ed3964984fdb477bb7a6c72126634b73
LeeHeeJun <secret1601@naver.com>
2024-09-27 15:05:26 +0900

    2024_09_27 - MISSION COMPLETE !!

--- js/test.js
+++ js/test.js
@@ -16,3 +16,45 @@ function setCameraTarget(x, y, z) {
 
     c
 }
+
+const copyArea = document.querySelectorAll(".copy-area");
+
+copyArea.forEach((area) => {
+    area.addEventListener("click", function () {
+        const numberToCopy = this.getAttribute('data-number');
+        console.log(numberToCopy);
+
+        const tempTextarea = document.createElement('textarea');
+        tempTextarea.value = numberToCopy;
+        document.body.appendChild(tempTextarea);
+
+        tempTextarea.select();
+        document.execCommand('copy');
+
+        document.body.removeChild(tempTextarea);
+
+        alert('복사된 번호: ' + numberToCopy);
+    });
+});
+
+const scrollContainer = document.querySelectorAll('.scroll-container');
+const scrollArea = document.querySelector('html');
+scrollContainer.forEach(container => {
+    container.addEventListener('wheel', (event) => {
+        event.preventDefault();
+
+        if (event.deltaY > 0) {
+            if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+                scrollArea.scrollTop += event.deltaY;
+            } else {
+                container.scrollLeft += event.deltaY;
+            }
+        } else {
+            if (container.scrollLeft <= 0) {
+                scrollArea.scrollTop += event.deltaY;
+            } else {
+                container.scrollLeft += event.deltaY;
+            }
+        }
+    });
+})
